776fda3d2bfc2f87ded441f74893d9a387b614d5
HeroBuxx <me@herobuxx.me>
2023-04-13 22:18:39 +0000

    tulip: Drop old camera libraries and unused shims
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- extract-files.sh
+++ extract-files.sh
@@ -6,16 +6,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/lib/hw/camera.sdm660.so)
-            for LIBCAMERA_SDM660_SHIM in $(grep -L "libcamera_sdm660_shim.so" "${2}"); do
-                "${PATCHELF}" --add-needed "libcamera_sdm660_shim.so" "$LIBCAMERA_SDM660_SHIM"
-            done
-            ;;
-    esac
-}
-
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
